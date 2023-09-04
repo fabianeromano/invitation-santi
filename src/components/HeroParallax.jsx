8690b74ca07b086643b5b66e7478b4091487ac42
fabianeromano <fabian.ed.romano@gmail.com>
2023-09-04 11:33:52 -0300

    fix horas

--- src/components/HeroParallax.jsx
+++ src/components/HeroParallax.jsx
@@ -5,8 +5,33 @@ import Santi from "/public/images/santi-title.svg"
 import Arcoiris from "/public/images/arcoiris.png"
 import Arrow from "/public/images/arrow.svg"
 import Link from 'next/link'
+import moment from 'moment';
+import { useState, useEffect } from 'react';
 
 function HeroParallax() {
+
+    const eventDate = moment('2023-09-12'); // Fecha y hora del evento
+
+    const [remainingTime, setRemainingTime] = useState({
+        days: 0
+    });
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = moment();
+            const duration = moment.duration(eventDate.diff(now));
+
+            setRemainingTime({
+                days: duration.days(),
+            });
+
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
     return (
         <section className='w-full h-screen min-h-[700px] relative flex flex-col items-center justify-start bg-[#F0FEFC] parallax'>
             <div className='z-10 pt-4'>
@@ -24,7 +49,7 @@ function HeroParallax() {
                     <span className='w-1 rounded-full h-[60%] bg-[#2176AE]' />
                     <div className='flex flex-col items-center justify-between h-full py-4'>
                         <p className='text-4xl font-medium text-[#4E5340]'>Faltan</p>
-                        <p className="text-4xl font-bold text-[#4E5340]">12 dias</p>
+                        <p className="text-4xl font-bold text-[#4E5340]">{remainingTime?.days} dias</p>
                     </div>
                 </div>
             </div>
