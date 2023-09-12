df9fc4a406b13e4b4dbbeb5f748ef1a117ebcf54
fabianeromano <fabian.ed.romano@gmail.com>
2023-09-12 16:23:11 -0300

    fix numero

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -12,7 +12,7 @@ function Location() {
             </div>
             <div className="relative flex flex-col items-center justify-start w-full gap-2 text-center">
                 <h3 className="text-3xl font-bold text-[#4E5340]">Merienda</h3>
-                <p className="px-4 text-xl font-medium text-[#4E5340]">Te esperamos en Av. Mate de Luna 1850 (Local 10)</p>
+                <p className="px-4 text-xl font-medium text-[#4E5340]">Te esperamos en Av. Mate de Luna 2850 (Local 10)</p>
                 <p className="text-2xl font-bold text-[#2176AE]">18:00hs</p>
             </div>
         </section>
